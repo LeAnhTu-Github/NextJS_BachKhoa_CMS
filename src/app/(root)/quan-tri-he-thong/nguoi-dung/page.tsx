9589956d119a5666870eef9de72a56d2c0f65c6b
LeAnhTu-Github <anhtumc9ass@gmail.com>
2025-06-30 08:49:23 +0700

    fix-2

--- src/app/(root)/quan-tri-he-thong/nguoi-dung/page.tsx
+++ src/app/(root)/quan-tri-he-thong/nguoi-dung/page.tsx
@@ -1,172 +1,165 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import { User, UserListResponse } from "@/types/User";
-// import api from "@/services/api";
-// import ListUser from "@/components/User/ListUser";
-// import UserSearchForm from "@/components/User/UserSearchForm";
-// import CustomPagination from "@/components/ui/custom-pagination";
-// import ModalCreateUser from "@/components/User/ModalCreateUser";
-// import { toast } from "sonner";
-// import ModalEditUser from "@/components/User/ModalEditUser";
+"use client";
+import React, { useState, useEffect } from "react";
+import { User, UserListResponse } from "@/types/User";
+import api from "@/services/api";
+import ListUser from "@/components/User/ListUser";
+import UserSearchForm from "@/components/User/UserSearchForm";
+import CustomPagination from "@/components/ui/custom-pagination";
+import ModalCreateUser from "@/components/User/ModalCreateUser";
+import ModalEditUser from "@/components/User/ModalEditUser";
 
-// type FormValues = {
-//   fullName?: string;
-//   email?: string;
-//   status?: string;
-// };
+import { toast } from "sonner";
 
-// const UserManagement = () => {
-//   const [users, setUsers] = useState<User[]>([]);
-//   const [pageIndex, setPageIndex] = useState(1);
-//   const [pageSize, setPageSize] = useState(50);
-//   const [totalPages, setTotalPages] = useState(1);
-//   const [totalRecords, setTotalRecords] = useState(0);
-//   const [fullName, setFullName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [status, setStatus] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [openCreateModal, setOpenCreateModal] = useState(false);
-//   const [openEditModal, setOpenEditModal] = useState(false);
-//   const [selectedUser, setSelectedUser] = useState<User | null>(null);
-
-//   const fetchUsers = async (
-//     pageIndex: number,
-//     pageSize: number,
-//     fullName?: string,
-//     email?: string,
-//     status?: string
-//   ) => {
-//     setLoading(true);
-//     try {
-//       const res = await api.get<{ data: UserListResponse }>("/user/list", {
-//         params: { pageIndex, pageSize, fullName, email, status },
-//       });
-//       const { data, totalPages, totalRecords } = res.data.data;
-//       setUsers(data);
-//       setTotalPages(totalPages);
-//       setTotalRecords(totalRecords);
-//     } catch (error) {
-//       console.error("Error fetching users:", error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   const handleSearch = (values: FormValues) => {
-//     const searchPayload = Object.entries(values).reduce((acc, [key, value]) => {
-//       if (value && value !== "") {
-//         acc[key as keyof FormValues] = value;
-//       }
-//       return acc;
-//     }, {} as FormValues);
-
-//     setFullName(searchPayload.fullName || "");
-//     setEmail(searchPayload.email || "");
-//     setStatus(searchPayload.status || "");
-//     setPageIndex(1);
-//     fetchUsers(
-//       pageIndex,
-//       pageSize,
-//       searchPayload.fullName,
-//       searchPayload.email,
-//       searchPayload.status
-//     );
-//   };
-
-//   const handleRefresh = () => {
-//     setPageIndex(1);
-//     fetchUsers(pageIndex, pageSize);
-//   };
-
-//   const handleAdd = () => {
-//     setOpenCreateModal(true);
-//   };
-
-//   const handlePageChange = (newPageIndex: number) => {
-//     setPageIndex(newPageIndex);
-//     fetchUsers(newPageIndex, pageSize, fullName, email, status);
-//   };
-
-//   const handleChangePageSize = (newPageSize: number) => {
-//     setPageSize(newPageSize);
-//     fetchUsers(pageIndex, newPageSize, fullName, email, status);
-//   };
-
-//   const handleDelete = async (userId: number) => {
-//     const reponse = await api.delete(`/user/${userId}`);
-//     if (reponse.status >= 200 && reponse.status < 300) {
-//       toast.success("Xóa người dùng thành công");
-//     } else {
-//       toast.error("Xóa người dùng thất bại");
-//     }
-//     handleRefresh();
-//   };
-
-//   const handleEdit = (userId: number) => {
-//     const user = users.find((u) => u.id === userId);
-//     if (user) {
-//       setSelectedUser(user);
-//       setOpenEditModal(true);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchUsers(pageIndex, pageSize);
-//     return () => {
-//       setUsers([]);
-//     };
-//   }, [pageIndex, pageSize]);
-
-//   return (
-//     <div className="p-3 flex flex-col gap-3">
-//       <UserSearchForm
-//         userCount={totalRecords}
-//         onSearch={handleSearch}
-//         onRefresh={handleRefresh}
-//         onAdd={handleAdd}
-//       />
-//       <div className="w-full flex flex-col gap-3 max-h-[calc(100vh-200px)] overflow-y-auto">
-//         <ListUser
-//           users={users}
-//           isLoading={loading}
-//           onRefresh={handleRefresh}
-//           onDelete={handleDelete}
-//           onEdit={handleEdit}
-//         />
-//         <CustomPagination
-//           setTotalPages={setTotalPages}
-//           currentPage={pageIndex}
-//           totalPages={totalPages}
-//           pageSize={pageSize}
-//           onPageChange={handlePageChange}
-//           onPageSizeChange={handleChangePageSize}
-//         />
-//       </div>
-
-//       <ModalCreateUser
-//         open={openCreateModal}
-//         onOpenChange={setOpenCreateModal}
-//         onRefresh={handleRefresh}
-//       />
-//       {selectedUser && (
-//         <ModalEditUser
-//           open={openEditModal}
-//           onOpenChange={setOpenEditModal}
-//           onRefresh={handleRefresh}
-//           user={selectedUser}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserManagement;
-import React from 'react'
+type FormValues = {
+  fullName?: string;
+  email?: string;
+  status?: string;
+};
 
 const UserManagement = () => {
-  return (
-    <div>UserManagement</div>
-  )
-}
+  const [users, setUsers] = useState<User[]>([]);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [pageSize, setPageSize] = useState(50);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
-export default UserManagement
+  const fetchUsers = async (
+    pageIndex: number,
+    pageSize: number,
+    fullName?: string,
+    email?: string,
+    status?: string
+  ) => {
+    setLoading(true);
+    try {
+      const res = await api.get<{ data: UserListResponse }>("/user/list", {
+        params: { pageIndex, pageSize, fullName, email, status },
+      });
+      const { data, totalPages, totalRecords } = res.data.data;
+      setUsers(data);
+      setTotalPages(totalPages);
+      setTotalRecords(totalRecords);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = (values: FormValues) => {
+    const searchPayload = Object.entries(values).reduce((acc, [key, value]) => {
+      if (value && value !== "") {
+        acc[key as keyof FormValues] = value;
+      }
+      return acc;
+    }, {} as FormValues);
+
+    setFullName(searchPayload.fullName || "");
+    setEmail(searchPayload.email || "");
+    setStatus(searchPayload.status || "");
+    setPageIndex(1);
+    fetchUsers(
+      pageIndex,
+      pageSize,
+      searchPayload.fullName,
+      searchPayload.email,
+      searchPayload.status
+    );
+  };
+
+  const handleRefresh = () => {
+    setPageIndex(1);
+    fetchUsers(pageIndex, pageSize);
+  };
+
+  const handleAdd = () => {
+    setOpenCreateModal(true);
+  };
+
+  const handlePageChange = (newPageIndex: number) => {
+    setPageIndex(newPageIndex);
+    fetchUsers(newPageIndex, pageSize, fullName, email, status);
+  };
+
+  const handleChangePageSize = (newPageSize: number) => {
+    setPageSize(newPageSize);
+    fetchUsers(pageIndex, newPageSize, fullName, email, status);
+  };
+
+  const handleDelete = async (userId: number) => {
+    const reponse = await api.delete(`/user/${userId}`);
+    if (reponse.status >= 200 && reponse.status < 300) {
+      toast.success("Xóa người dùng thành công");
+    } else {
+      toast.error("Xóa người dùng thất bại");
+    }
+    handleRefresh();
+  };
+
+  const handleEdit = (userId: number) => {
+    const user = users.find((u) => u.id === userId);
+    if (user) {
+      setSelectedUser(user);
+      setOpenEditModal(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers(pageIndex, pageSize);
+    return () => {
+      setUsers([]);
+    };
+  }, [pageIndex, pageSize]);
+
+  return (
+    <div className="p-3 flex flex-col gap-3">
+      <UserSearchForm
+        userCount={totalRecords}
+        onSearch={handleSearch}
+        onRefresh={handleRefresh}
+        onAdd={handleAdd}
+      />
+      <div className="w-full flex flex-col gap-3 max-h-[calc(100vh-200px)] overflow-y-auto">
+        <ListUser
+          users={users}
+          isLoading={loading}
+          onRefresh={handleRefresh}
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+        />
+        <CustomPagination
+          setTotalPages={setTotalPages}
+          currentPage={pageIndex}
+          totalPages={totalPages}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+          onPageSizeChange={handleChangePageSize}
+        />
+      </div>
+
+      <ModalCreateUser
+        open={openCreateModal}
+        onOpenChange={setOpenCreateModal}
+        onRefresh={handleRefresh}
+      />
+      {selectedUser && (
+        <ModalEditUser
+          open={openEditModal}
+          onOpenChange={setOpenEditModal}
+          onRefresh={handleRefresh}
+          user={selectedUser}
+        />
+      )}
+    </div>
+  );
+};
+
+export default UserManagement;
+
